a58f884dd2643a04c0751678a90f3489233e2729
jiangyanyan <jiangyanyan@cnbn.cn>
2019-02-12 17:25:23 +0800

    服务提供者

---
+++ ms-root/ms-provider-system/src/main/java/com/tool/MsProviderSystemApplication.java
@@ -0,0 +1,16 @@
+package com.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsProviderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProviderSystemApplication.class, args);
+	}
+
+}
+
